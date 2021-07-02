cc504a6859fa4ae75a11bbaffe66189abd78cc4a
Анастасия Крутицкая <anastasia@MacBook-Pro-Anastasia.local>
2021-07-02 09:49:40 +0300

    commit

---
+++ src/js/_tests_/app.test.js
@@ -0,0 +1,6 @@
+import createCellsArray, { allCells } from '../app';
+
+test('', () => {
+  createCellsArray();
+  expect(allCells).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,39 @@
 // TODO: write code here
 // setInterval(console.log('hi'), 1000);
-
 const boardSize = 4;
-const container = document.querySelector('#game-container');
-container.innerHTML = `
-<div class="board-container">
-  <div data-id="board" class="board"></div>
-</div>`;
-
-const boardElement = container.querySelector('[data-id=board]');
-for (let i = 0; i < boardSize ** 2; i += 1) {
-  const cellEl = document.createElement('div');
-  cellEl.classList.add('cell');
-  boardElement.appendChild(cellEl);
-}
-
-const allCells = [];
-for (let i = 0; i < boardSize ** 2; i += 1) {
-  allCells.push(i);
+export const allCells = [];
+export default function createCellsArray() {
+  for (let i = 0; i < boardSize ** 2; i += 1) {
+    allCells.push(i);
+  }
 }
 document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('#game-container');
+  document.querySelector('#game-container').innerHTML = `
+  <div class="board-container">
+    <div data-id="board" class="board"></div>
+  </div>`;
+
+  const boardElement = container.querySelector('[data-id=board]');
+  for (let i = 0; i < boardSize ** 2; i += 1) {
+    const cellEl = document.createElement('div');
+    cellEl.classList.add('cell');
+    boardElement.appendChild(cellEl);
+  }
+  createCellsArray();
   const newPosition = allCells[Math.floor(Math.random() * allCells.length)];
   const cell = boardElement.children[newPosition];
   cell.classList.add('goblin');
 });
 
 setInterval(() => {
-  const goblin = Array.from(boardElement.querySelectorAll('.cell')).findIndex((item) => item.classList.contains('goblin'));
+  const goblin = Array.from(document.querySelector('[data-id=board]').querySelectorAll('.cell')).findIndex((item) => item.classList.contains('goblin'));
   let newPosition = allCells[Math.floor(Math.random() * allCells.length)];
   do {
     newPosition = allCells[Math.floor(Math.random() * allCells.length)];
   }
   while (goblin === newPosition);
-  const cell = boardElement.children[newPosition];
+  const cell = document.querySelector('[data-id=board]').children[newPosition];
   if (document.querySelector('.goblin')) {
     document.querySelector('.goblin').classList.remove('goblin');
   }
